dd5cd7316f0c008bbb81b5397f9acd8795561546
Hyeonseok Jeong <thema00000@naver.com>
2025-05-08 16:38:46 +0900

    test임

---
+++ database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "mysql+mysqlconnector://root:5749@localhost:3306/seating_test"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
